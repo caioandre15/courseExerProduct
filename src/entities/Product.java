98fbea265ca48f09ebba8674cbbf2606d68ec996
Caio André <caio_andre15@hotmail.com>
2020-08-03 21:07:29 -0300

    Created the Product class in the entities package

---
+++ src/entities/Product.java
@@ -0,0 +1,45 @@
+package entities;
+
+public class Product {
+
+	// Attributes
+	private String name;
+	private Double price;
+
+	// Builders
+	public Product() {
+	}
+
+	public Product(String name, Double price) {
+		this.name = name;
+		this.price = price;
+	}
+
+	// Accessor Methods
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+	
+	//Methods
+	public String priceTag() {
+		String quebraLinha = System.getProperty("line.separator");
+		return "Name: " 
+				+ name 
+				+ quebraLinha
+				+ "Price: " 
+				+ price;
+	}
+
+}
